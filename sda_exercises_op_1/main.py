f79c066adcaa90bac0febbda4b88b3ec2d3714c6
JedrzejSierpinski <jedrzej.sierpinski@gmail.com>
2021-01-31 13:19:32 +0100

    added sound atribute

--- sda_exercises_op_1/main.py
+++ sda_exercises_op_1/main.py
@@ -3,11 +3,25 @@ from python_intermidiate_training.sda_exercises_op_1.cat import Cat
 
 def main():
     cat_object = Cat("Filemon")
-    sound = cat_object.make_sound()
-    print(sound)
+    cat_object_2 = Cat("Mruczek", "miałmiał")
+    cat_object_3 = Cat("Pipak")
+    cat_object_4 = Cat("Jarosław")
+
+    # cats = [cat_object, cat_object_2, cat_object_3, cat_object_4]
+    cats = []
+    cats.append(cat_object)
+    cats.append(cat_object_2)
+    cats.append(cat_object_3)
+    cats.append(cat_object_4)
+
+    for cat in cats:
+        sound = cat.make_sound()
+        print(sound)
+
 
 # zmienna sound jest stringiem
 
 
 if __name__ == "__main__":
     main()
+
