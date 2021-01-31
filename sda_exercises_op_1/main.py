dd6e7776a81d854805310c1e2a2ac7b8b54c3e88
JedrzejSierpinski <jedrzej.sierpinski@gmail.com>
2021-01-31 13:00:57 +0100

    created def cat

--- sda_exercises_op_1/main.py
+++ sda_exercises_op_1/main.py
@@ -1,5 +1,12 @@
+from python_intermidiate_training.sda_exercises_op_1.cat import Cat
+
+
 def main():
-    print("hello world")
+    cat_object = Cat("Filemon")
+    sound = cat_object.make_sound()
+    print(sound)
+
+# zmienna sound jest stringiem
 
 
 if __name__ == "__main__":
