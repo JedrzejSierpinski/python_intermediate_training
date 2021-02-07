14d3b7da843b636a00ad76d81944d9c586bcdbfe
JedrzejSierpinski <jedrzej.sierpinski@gmail.com>
2021-02-07 13:40:32 +0100

    users were addes

---
+++ sda_serialize/csv_training.py
@@ -0,0 +1,42 @@
+import csv
+
+
+def csv_write(users_list):
+    try:
+        with open('./data.csv', 'w') as fd:
+            writer = csv.writer(fd)
+            for user_tuple in users_list:
+                writer.writerow(user_tuple)
+
+    except (IOError, Exception) as e:
+        print(f'Exception while writing csv format info: {e.args}')
+
+
+def csv_read():
+    users = []
+    try:
+        with open('./data.csv', 'r') as fd:
+            reader = csv.reader(fd)
+            for row in reader:
+                if row:
+                    users.append(row)
+
+
+    except (IOError, Exception) as e:
+        print(f'Exception while writing csv format info: {e.args}')
+
+    return users
+
+
+def main():
+    users = [
+        ("Jaroslaw", "Kaczynski", 11111111),
+        ("Lech", "Kaczynski", 11111111)
+    ]
+
+    csv_write(users)
+    returned_users = csv_read()
+    print(returned_users)
+
+if __name__ == "__main__":
+    main()
--- sda_serialize/pickle_training.py
+++ sda_serialize/pickle_training.py
@@ -10,3 +10,24 @@ def pickle_write(items: list):
             pickle.dumps(items, fd)
     except (IOError, Exception) as e:
         print(f'Exception while writing pickle format info: {e.args}')
+    print(f'Pickle write finish')
+
+def pickle_read(items: list):
+    string_list = []
+    try:
+        with open('./data.pickle', 'wb') as fd:
+            pickle.dump(items, fd)
+            pickle.dumps(items, fd)
+    except (IOError, Exception) as e:
+        print(f'Exception while writing pickle format info: {e.args}')
+
+
+def main():
+    abc = ['bread', 'butter', 'cola']
+    pickle_write(abc)
+    list_of_strings = pickle_read()
+    print(list_of_strings)
+
+
+if __name__ == "__main__":
+    main()
