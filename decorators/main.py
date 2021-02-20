6478bfeec2aa629693909f95be133b9208702ca4
JedrzejSierpinski <jedrzej.sierpinski@gmail.com>
2021-02-20 13:26:14 +0100

    Wraps decorator was add

--- decorators/main.py
+++ decorators/main.py
@@ -1,4 +1,19 @@
+from functools import wraps
+
+
 def catch_io_error_decorator(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IOError as e:
+            print(f"IOError cougth it {e.args}")
+            return None
+
+    return inner
+
+
+def catch_io_error_with_wraps(func):
+    @wraps(func) #zapamiętuje ścieżkę wywołań!111111
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -14,8 +29,15 @@ def throw_exception_file():
     raise IOError("Test error")
 
 
+@catch_io_error_with_wraps
+def read_does_not_exist_file():
+    with open("./training.training.training", "r") as fd:
+        fd.read()
+
+
 def main():
-    throw_exception_file()
+    # throw_exception_file()
+    read_does_not_exist_file()
 
 
 if __name__ == "__main__":
